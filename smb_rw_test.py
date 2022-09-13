b710f979c471f20a3224821f4ab1342c97647013
ddl-viniatska <69326214+ddl-viniatska@users.noreply.github.com>
2022-09-13 16:39:29 +0000

    Create smb_rw_test.py

---
+++ smb_rw_test.py
@@ -0,0 +1,27 @@
+import os
+import uuid
+
+unique_filename = str(uuid.uuid4())
+
+# Specify the directory path
+path = r'/domino/edv/diabetes_rw'
+
+file_name = unique_filename
+
+# Print file to be created
+print(unique_filename)
+
+# Creating a file at specified folder
+with open(os.path.join(path, file_name), 'w') as fp:
+    fp.write('Test passed')
+
+print(os.listdir(path))
+
+# verify file exist
+filepath = os.path.join(path,unique_filename)
+print(os.path.isfile(filepath))
+
+
+# print the contents of a file
+with open(filepath, 'r') as f:
+    print(f.read())
